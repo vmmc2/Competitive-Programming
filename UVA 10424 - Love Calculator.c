a6c8a99c2d1ecd515c0473ef4e819c15dd89e1d6
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2018-12-26 06:33:22 -0500

    Update and rename love calculator (errado) to UVA 10424 - Love Calculator.c

--- UVA 10424 - Love Calculator.c
+++ UVA 10424 - Love Calculator.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <ctype.h>
 
-
 int random(char str[]){
     int number, sum;
+    int i;
     while(strlen(str) != 1){
-            for(int i = 0; i < strlen(str); i++){
+            for(i = 0; i < strlen(str); i++){
                 sum += str[i] - 48;
             }
             sprintf(str,"%d",sum);
@@ -23,23 +23,24 @@ int main(){
     char aux1[50], aux2[50];
     int answer1, answer2;
     double final1, final2;
+    int i;
     while(gets(nome1)){
         gets(nome2);
         number1 = 0, number2 = 0;
-        for(int i = 0; i < strlen(nome1); i++){
+        for(i = 0; i < strlen(nome1); i++){
             nome1[i] = tolower(nome1[i]);
         }
-        for(int i = 0; i < strlen(nome2); i++){
+        for(i = 0; i < strlen(nome2); i++){
             nome2[i] = tolower(nome2[i]);
         }
-        for(int i = 0; i < strlen(nome1); i++){
+        for(i = 0; i < strlen(nome1); i++){
             if((int)nome1[i] >= 97 && (int)nome1[i] <= 122){
                 number1 += (nome1[i]-96);
             }    
         }
         sprintf(aux1, "%d", number1);
         answer1 = random(aux1);
-        for(int i = 0; i < strlen(nome2); i++){
+        for(i = 0; i < strlen(nome2); i++){
             if((int)nome2[i] >= 97 && (int)nome2[i] <= 122){
                 number2 += (nome2[i]-96);
             }
