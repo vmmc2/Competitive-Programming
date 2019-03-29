24b9dcbac3d406d4bed167bc5a18397d31c076fd
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-03-28 23:03:58 -0400

    Update uva 200 - incompleto.cpp

--- uva 200 - incompleto.cpp
+++ uva 200 - incompleto.cpp
@@ -22,15 +22,21 @@ void toposort(int x){
 
 pair<char,char> find(string auxiliar1, string auxiliar2){
     pair<char, char> dale;
+    /*if(auxiliar2.length() > auxiliar1.length()){
+        dale.first = auxiliar1[auxiliar1.length() - 1];
+        dale.second = auxiliar2[auxiliar2.length() - 1];
+        return dale;
+    }*/
     int limite = min(auxiliar1.length(), auxiliar2.length());
     for(int i = 0; i < limite; i++){
         if(auxiliar1[i] != auxiliar2[i]){
             dale.first = auxiliar1[i];
             dale.second = auxiliar2[i];
-            break;
+            return dale;
         }
     }
-    
+    dale.first = auxiliar1[auxiliar1.length() - 1];
+    dale.second = auxiliar2[auxiliar2.length() - 1];
     return dale;
 }
 
@@ -41,30 +47,36 @@ int main(){
     pair<char,char> ordem;
     while(true){
         cin >> aux;
-        if(aux == "#")break;
+        if(aux == "#"){
+            break;
+        }
         registro.push_back(aux);
     }
-    for(int i = 0; i <= (int)registro.size()-2; i++){
+    for(int i = 0; i <= (int)registro.size() - 2; i++){
         ordem = find(registro[i], registro[i + 1]);
-        if(dictionary.find(ordem.first) != dictionary.end()){
+        //cout << ordem.first << " " << ordem.second << endl;
+        if(dictionary.find(ordem.first) == dictionary.end()){
             dictionary[ordem.first] = chave;
             dic2[chave] = ordem.first;
             chave++;
         }
-        if(dictionary.find(ordem.second) != dictionary.end()){
+        if(dictionary.find(ordem.second) == dictionary.end()){
             dictionary[ordem.second] = chave;
             dic2[chave] = ordem.second;
             chave++;
         }
         adjlist[dictionary[ordem.first]].push_back(dictionary[ordem.second]);
     }
+    /*for(map<char,int>::iterator it = dictionary.begin(); it != dictionary.end(); ++it){
+        cout << it->first << " " << it->second << endl;
+    }*/
     for(int i = 1; i <= chave - 1; i++){
         if(visitados[i] == 0){
             toposort(i);
         }
     }
-    /*for(int a = (int)answer.size() - 1; a >= 0; a--){
+    for(int a = (int)answer.size() - 1; a >= 0; a--){
         printf("%c", answer[a]);
-    }*/
+    }
     return 0;
 }
