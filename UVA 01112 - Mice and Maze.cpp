87e7deabf5d3d2771a770eeaeae1984c9de73cf0
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-07-15 14:07:54 -0400

    Update UVA 01112 - Mice and Maze.cpp

--- UVA 01112 - Mice and Maze.cpp
+++ UVA 01112 - Mice and Maze.cpp
@@ -1,56 +1,61 @@
 #include <bits/stdc++.h>
-#define MAXN 110
-#define INF 99999999
+#include <cmath>
+#define INF 0xffffff
 
 using namespace std;
-int adjmatrix[MAXN][MAXN];
+
+int adjmatrix[110][110];
+int numvertices;
 
 void preprocess(){
-    int i, j;
-    for(i = 0; i <= MAXN; i++){
-        for(j = 0; j <= MAXN; j++){
+    for(int i = 0; i < 110; i++){
+        for(int j = 0; j < 110; j++){
             adjmatrix[i][j] = INF;
+            if(i == j){
+                adjmatrix[i][j] = 0;
+            }
         }
     }
 }
 
-void FloydWarshall(int n){
-    int i, j, k;
-    for(k = 1; k <= n; k++){
-        for(i = 1; i <= n; i++){
-            for(j = 1; j <= n; j++){
-                adjmatrix[i][j] = min(adjmatrix[i][j], adjmatrix[i][k]+adjmatrix[k][j]);
+void floyd_warshall(){
+    for(int k = 1; k <= numvertices; k++){
+        for(int i = 1; i <= numvertices; i++){
+            for(int j = 1; j <= numvertices; j++){
+                adjmatrix[i][j] = min(adjmatrix[i][j], adjmatrix[i][k] + adjmatrix[k][j]);
             }
         }
     }
 }
 
 int main(){
-    int numcasos, i, j;
-    int a, b, w;
-    int chegada;
-    int limite, ratinhos;
-    int n, m;//n = numero de vertices, m = numero de arestas
+    int numcasos;
+    int numedges, tle;
+    int numexit;
+    int a, b, weight;
+    int rats;
     scanf("%d", &numcasos);
-    for(i = 1; i <= numcasos; i++){
+    while(numcasos--){
         preprocess();
-        cin.ignore();
-        scanf("%d", &n);
-        ratinhos = n;
-        scanf("%d", &chegada);
-        scanf("%d", &limite);
-        scanf("%d", &m);
-        for(j = 1; j <= m; j++){
-            scanf("%d %d %d", &a, &b, &w);
-            adjmatrix[a][b] = w;
+        rats = 0;
+        scanf("%d", &numvertices);
+        scanf("%d", &numexit);
+        scanf("%d", &tle);
+        scanf("%d", &numedges);
+        for(int i = 1 ; i <= numedges; i++){
+            scanf("%d %d %d", &a, &b, &weight);
+            adjmatrix[a][b] = weight;
         }
-        FloydWarshall(n);
-        for(j = 1; j <= n; j++){
-            if(limite < adjmatrix[j][chegada]){
-               ratinhos--;                                       
+        floyd_warshall();
+        for(int i = 1; i <= numvertices; i++){
+            if(adjmatrix[i][numexit] <= tle || i == numexit){
+                rats++;
             }
         }
-        printf("%d\n", ratinhos+1);
+        printf("%d\n", rats);
+        if(numcasos){
+            printf("\n");
+        }
     }
     return 0;
 }
