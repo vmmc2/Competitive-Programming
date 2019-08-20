7b686b186ec6d296e33de05e82cebc64aa79a006
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-08-19 22:56:30 -0300

    Create Tricks_3.cpp

---
+++ Tricks_3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#include <cmath>
+#define INF 9999999
+#define pb push_back
+#define pii pair<int,int>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	//Sorting is a fundamental algorithm design problem. Many efficient algorithms
+	//use sorting as a subroutine, because it is often easier to process data if the
+	//elements are in a sorted order.
+
+	//The efficient general sorting
+	//algorithms work in O (n*log(n)) time, and many algorithms that use sorting as a
+	//subroutine also have this time complexity.
+
+	//Algoritmo de Ordenacao Bubble-Sort ----- TIME COMPLEXITY: O(n2)
+	//Algoritmo de Ordenacao Selection-Sort -- TIME COMPLEXITY: O(n2)
+
+
+
+	return 0;
+}
