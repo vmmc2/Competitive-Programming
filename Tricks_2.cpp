2f82c4aef3df43dd4227ac5999d2fd9d8d7753e5
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-08-19 22:28:58 -0300

    Create Tricks_2.cpp

---
+++ Tricks_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#include <cmath>
+#define INF 9999999
+#define pb push_back
+#define pii pair<int,int>
+
+using namespace std;
+
+int main(){
+	//TIME COMPLEXITY CHEAT SHEET...
+	//SUPONHA QUE O TIME LIMIT DE UM PROBLEMA SEJA DE 1 SEGUNDO. ENTAO, TEMOS O SEGUINTE:
+	INPUT SIZE ------------------ REQUIRED TIME COMPLEXITY
+	  n <= 10                             O(n!)
+	  n <= 20                             O(2eN)
+	  n <= 500                            O(n3)
+	  n <= 5000                           O(n2)
+	  n <= 10e6                           O(n) or O(n*log(n))
+	  n is large                          O(1) or O(log(n))
+
+	  
+	return 0;
+}
