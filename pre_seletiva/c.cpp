bf10bf31eb3bff7542b48b591d4bf1a26730777b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-08-27 22:31:55 -0300

    Update c.cpp

--- pre_seletiva/c.cpp
+++ pre_seletiva/c.cpp
@@ -11,6 +11,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 struct player{
+    string real_name;
     string nome;
     int points;
 };
@@ -31,12 +32,13 @@ int main(){
         for(int i = 0; i < numplayers; i++){
             int pontos = 0;
             string name;
-            string lixeira
-            getline(cin, lixeira);
-            stringstream ss;
-            //int v[25];
-            //char nome[100];
-            //scanf("%[^\n]s ;%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d",nome, &v[0], &v[1], &v[2], &v[3], &v[4], &v[5], &v[6], &v[7], &v[8], &v[9], &v[10], &v[11], &v[12], &v[13], &v[14], &v[15], &v[16], &v[17], &v[18], &v[19], &v[20], &v[21], &v[22], &v[23], &v[24]);
+            string aux_name = "";
+            //string lixeira
+            //getline(cin, lixeira);
+            //stringstream ss;
+            int v[25];
+            char nome[100];
+            scanf("%[^;];%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d;%d %d %d %d %d",nome, &v[0], &v[1], &v[2], &v[3], &v[4], &v[5], &v[6], &v[7], &v[8], &v[9], &v[10], &v[11], &v[12], &v[13], &v[14], &v[15], &v[16], &v[17], &v[18], &v[19], &v[20], &v[21], &v[22], &v[23], &v[24]);
             //printf("%s\n", nome);
             for(int i = 0; i < 25; i++){
                 if(i == 4 || i == 9 || i == 14 || i == 19 || i == 24){
@@ -46,6 +48,7 @@ int main(){
                 }
             }
             for(int i = 0; i < strlen(nome); i++){
+                aux_name += nome[i];
                 if(nome[i] >= 65 && nome[i] <= 90){
                     name += (char)(nome[i] + 32);
                 }else{
@@ -54,14 +57,20 @@ int main(){
             }
             player aux;
             aux.nome = name;
+            aux.real_name = aux_name;
             aux.points = pontos;
             registro.pb(aux);
         }
         sort(registro.begin(), registro.end(), compare);
-        //printf("Case %d:\n", caso);
-        /* for(int i = 0; i < (int)registro.size(); i++){
-            cout << registro[i].nome << " " << registro[i].points << "\n";
-        }*/
+        if(caso == 1){
+            printf("Case %d:", caso);
+        }else{
+            printf("Case %d:", caso);
+        }
+        for(int i = 0; i < (int)registro.size(); i++){
+            cout << registro[i].real_name << " " << registro[i].points;
+        }
+        printf("\n");
         caso++;
     }
     return 0;
