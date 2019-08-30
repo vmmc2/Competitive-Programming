d0d0ba0448974cd3d7289004b158f11f14fc70b1
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-08-30 20:25:42 -0300

    Update and rename uva - big screen.cpp to UVA - Giant Screen.cpp

--- pre_seletiva/UVA - Giant Screen.cpp
+++ pre_seletiva/UVA - Giant Screen.cpp
@@ -2,14 +2,15 @@
 #include <sstream>
 #include <cmath>
 #define pb push_back
-#define INF 999999
+#define INF 9999999
 
 using namespace std;
 
 typedef long long ll;
+typedef unsigned long long ull;
 
 int main(){
-	int xdes, ydes, rxdes, rydes;
+    int xdes, ydes, rxdes, rydes;
 	int n;
 	ll bestprice = 999999999999;
 	while(scanf("%d %d %d %d", &xdes, &ydes, &rxdes, &rydes) == 4){
@@ -20,10 +21,7 @@ int main(){
 		for(int i = 0; i < n; i++){
 			scanf("%d %d %d %d %lld", &x, &y, &rx, &ry, &price);
 			int numh = 0, numv = 0;
-			/*while(true){
-				/*if(x >= xdes && y >= ydes && rx >= rxdes && ry >= rydes){
-
-				}*/
+			//testando na horizontal normalmente...
 			int numx, numx1, numy, numy1;
 			if(xdes%x == 0){
 				numx = xdes/x;
@@ -49,17 +47,41 @@ int main(){
 				numy1 = rydes/ry;
 				numy1++;
 			}
-			/*int numy = ceil((double)ydes/y);
-			int numx1 = ceil((double)rxdes/rx);
-			int numy1 = ceil((double)rydes/ry);*/
-			///printf("%d %d %d %d %lld\n", numx, numy, numx1, numy1, price);
 			numx = max(numx, numx1);
 			numy = max(numy, numy1);
 			ll finalprice = (ll)(numx*numy)*price;
-			bestprice = min(bestprice, finalprice);
+			//testando agora o role na vertical....
+			int numx2, numx3, numy2, numy3;
+			if(xdes%y == 0){
+			    numx2 = xdes/y;
+			}else{
+			    numx2 = xdes/y;
+			    numx2++;
+			}
+			if(ydes%x == 0){
+			    numy2 = ydes/x;
+			}else{
+			    numy2 = ydes/x;
+			    numy2++;
+			}
+			if(rxdes%ry == 0){
+			    numx3 = rxdes/ry;
+			}else{
+			    numx3 = rxdes/ry;
+			    numx3++;
+			}
+			if(rydes%rx == 0){
+			    numy3 = rydes/rx;
+			}else{
+			    numy3 = rydes/rx;
+			    numy3++;
+			}
+			int nx = max(numx2, numx3);
+			int ny = max(numy2, numy3);
+			ll fp2 = (ll)(nx*ny)*price;
+			bestprice = min(bestprice,min(finalprice, fp2));
 		}
 		printf("%lld\n", bestprice);
-		
 	}
-	return 0;
+    return 0;
 }
