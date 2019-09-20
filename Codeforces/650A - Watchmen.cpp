de69cbf5ffb9f718102a548145a4f25b1fb7235b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-09-20 12:24:54 -0400

    Update and rename watchmen - (arrumar).cpp to 650A - Watchmen.cpp

--- Codeforces/650A - Watchmen.cpp
+++ Codeforces/650A - Watchmen.cpp
@@ -9,12 +9,12 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
  
-int comb(int n, int k){
+ll comb(int n, int k){
     double res = 1;
     for(int i = 1; i <= k; ++i){
         res = res * (n - k + i) / i;
     }
-    return (int)(res + 0.01);
+    return (ll)(res + 0.01);
 }
  
 int main(){
@@ -25,16 +25,13 @@ int main(){
     cin >> watchmen;
     map<int,int> x;
     map<int,int> y;
-    set<pair<int,int>> ss;
+    map<pair<int,int>,int> ss;
     int x0, y0;
     for(int i = 1; i <= watchmen; i++){
         cin >> x0 >> y0;
-        if(ss.find({x0,y0}) == ss.end()){
-            printf("entrei\n");
-            ss.insert({x0,y0});
-            x[x0]++;
-            y[y0]++;
-        }
+        ss[{x0,y0}]++;
+        x[x0]++;
+        y[y0]++;
     }
     ll ans = 0;
     for(map<int,int>::iterator it = x.begin(); it != x.end(); ++it){
@@ -45,6 +42,11 @@ int main(){
     for(map<int,int>::iterator it = y.begin(); it != y.end(); ++it){
         if(it->second >= 2){
             ans += comb(it->second,2)*1LL;
+        }
+    }
+    for(map<pair<int,int>,int>::iterator it = ss.begin(); it != ss.end(); ++it){
+        if(it->second > 1){
+            ans -= comb(it->second, 2);
         }
     }
     cout << ans << "\n";
