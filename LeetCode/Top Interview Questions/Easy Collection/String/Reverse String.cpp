0418d0a2f9db556f34f146e70030058510b223b9
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-09-29 11:58:21 -0300

    Update Reverse String.cpp

--- LeetCode/Top Interview Questions/Easy Collection/String/Reverse String.cpp
+++ LeetCode/Top Interview Questions/Easy Collection/String/Reverse String.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        //Primeira coisa: Verificar se o tamanho da string eh par ou impar
+        int tam = (int)s.size();
+        if(tam % 2 == 0){//tamanho par
+            int inicio = 0;
+            int fim = (int)s.size() - 1;
+            char aux;
+            while(inicio < tam/2){
+                aux = s[inicio];
+                s[inicio] = s[fim];
+                s[fim] = aux;
+                inicio++;
+                fim--;
+            }
+        }else{//tamanho impar
+            int inicio = 0;
+            int fim = (int)s.size() - 1;
+            char aux;
+            while(inicio < tam/2){
+                aux = s[inicio];
+                s[inicio] = s[fim];
+                s[fim] = aux;
+                inicio++;
+                fim--;
+            }
+        }
+    }
+};
