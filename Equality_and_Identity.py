39ec29dfa3427c6faf3d7a1112e9100723e8ca50
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-09-30 23:52:25 -0300

    Update Equality_and_Identity.py

--- Equality_and_Identity.py
+++ Equality_and_Identity.py
@@ -1,3 +1,4 @@
 What is the difference between "==" and "is"?
 1) "==" checks for equality between objects 
 2) "is" checks for identity. In other words, it checks if a specific object belongs to a certain type or class.
+Identity also checks if we are talking about the same object present in the memory.
