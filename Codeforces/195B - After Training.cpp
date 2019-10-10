618e17f1dcfe4d751159c6d0e0bfeffd781432a8
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-10-10 07:23:37 -0300

    Update and rename after training (certo?) to 195B - After Training.cpp

--- Codeforces/195B - After Training.cpp
+++ Codeforces/195B - After Training.cpp
@@ -3,41 +3,42 @@
 #include <sstream>
 #define pb push_back
 #define INF 99999999
-
+ 
 using namespace std;
-
+ 
 typedef long long ll;
 typedef unsigned long long ull;
-
-int middle;
-
-int dist(int meio, int index){
+ 
+double middle;
+ 
+double dist(double meio, int index){
+    double ind = (double)index;
     if(meio >= index){
-        return (meio - index);
+        return (double)(meio - ind);
     }else{
-        return (index - meio);
+        return (double)(ind - meio);
     }
 }
-
+ 
 struct comp{
     bool operator()(pair<int,int> a, pair<int,int> b){
         if(a.first == b.first){//se a qtd de bolas no cesto for a msm...
             if(dist(middle, a.second) == dist(middle, b.second)){//se a distancia dos cestos for igual...
-                return a.second < b.second; //olho pro indice.
+                return a.second > b.second; //olho pro indice.
             }else if(dist(middle, a.second) < dist(middle, b.second)){
-                return true;
-            }else{
                 return false;
+            }else{
+                return true;
             }
         }
         else if(a.first < b.first){
-            return true;
-        }else if(a.first > b.first){
             return false;
+        }else if(a.first > b.first){
+            return true;
         }
     }
 };
-
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -46,12 +47,7 @@ int main(){
     cin >> m >> n;
     //m = number of balls;
     //n = number of baskets;
-    if(n == 1){
-        for(int i = 0; i < m; i++){
-            cout << "1" << "\n";
-        }
-    }
-    middle = (1 + n)/2;
+    middle = (double)(1 + n)/2;
     priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq;
     register int x = 1;
     for(int i = 1; i <= n; i++){
