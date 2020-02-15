c46d421c7cb5a4da42d1bc269101b6c3fab318b9
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-02-15 18:38:50 -0300

    Create Delete a Node In Linked List O(1).cpp

---
+++ LeetCode/Top Interview Questions/Easy Collection/Linked List/Delete a Node In Linked List O(1).cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Time and Space Complexity are both: O(1).
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+        return;
+    }
+};
