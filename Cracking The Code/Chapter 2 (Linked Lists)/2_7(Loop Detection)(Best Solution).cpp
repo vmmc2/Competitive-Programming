d8f1bd12f0e399c8f346b12742e2dfed8ade24fe
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-01 13:25:22 -0700

    Create 2_7(Loop Detection)(Best Solution).cpp

---
+++ Cracking The Code/Chapter 2 (Linked Lists)/2_7(Loop Detection)(Best Solution).cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <set>
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a_pointer = headA;
+        ListNode *b_pointer = headB;
+        
+        while(a_pointer != b_pointer){
+            if(a_pointer == NULL){
+                a_pointer = headB;
+            }else{
+                a_pointer = a_pointer->next;
+            }
+            
+            if(b_pointer == NULL){
+                b_pointer = headA;
+            }else{
+                b_pointer = b_pointer->next;
+            }
+        }
+        return a_pointer;
+    }
+};
