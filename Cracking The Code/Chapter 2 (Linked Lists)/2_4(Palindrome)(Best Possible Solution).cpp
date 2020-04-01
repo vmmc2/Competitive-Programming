45f1aac09e18fc8ba2148d1e9d36b7e18b832e9e
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-01 11:52:57 -0700

    Update 2_4(Palindrome)(Best Possible Solution).cpp

--- Cracking The Code/Chapter 2 (Linked Lists)/2_4(Palindrome)(Best Possible Solution).cpp
+++ Cracking The Code/Chapter 2 (Linked Lists)/2_4(Palindrome)(Best Possible Solution).cpp
@@ -8,7 +8,7 @@
  */
 class Solution {
 public:
-    ListNode* reversed(ListNode *head){
+    ListNode* reverse_linked_list(ListNode *head){
         ListNode *prev = NULL;
         while(head != NULL){
             ListNode *temp = head->next;
@@ -18,19 +18,31 @@ public:
         }
         return prev;
     }
+    void print_list(ListNode* head){
+        while(head != NULL){
+            cout << head->val << " -> ";
+            head = head->next;
+        }
+        cout << "NULL" << "\n";
+        return;
+    }
+    
     bool isPalindrome(ListNode* head) {
+        //The "slow" pointer is going to move one node per iteration while the "fast" node will move two nodes
+        //per iteration.
         ListNode *slow = head;
         ListNode *fast = head;
-        
         while(fast != NULL && fast->next != NULL){
             fast = fast->next->next;
             slow = slow->next;
         }
-        
+        //After that, we reset the fast pointer
         fast = head;
-        slow = reversed(slow);
-        
+        slow = reverse_linked_list(slow); //Here we created a new list and assigned it to slow.
+        print_list(slow);
+        //And we just do a checking, comparing the correspondent nodes.
         while(slow != NULL){
+            cout << fast->val << " " << slow->val << "\n";
             if(fast->val != slow->val) return false;
             fast = fast->next;
             slow = slow->next;
