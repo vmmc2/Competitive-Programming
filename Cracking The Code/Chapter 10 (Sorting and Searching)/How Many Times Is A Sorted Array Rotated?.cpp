add863096b2ef0a19af26c14eed07ab4b755babe
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-03 15:53:16 -0700

    Update How Many Times Is A Sorted Array Rotated?.cpp

--- Cracking The Code/Chapter 10 (Sorting and Searching)/How Many Times Is A Sorted Array Rotated?.cpp
+++ Cracking The Code/Chapter 10 (Sorting and Searching)/How Many Times Is A Sorted Array Rotated?.cpp
@@ -1,34 +1,37 @@
 #include <bits/stdc++.h>
+#include <sstream>
+#include <cmath>
 
 using namespace std;
 
-int find_rotation(vector<int> &v){
+
+//O algoritmo abaixo so funciona no caso de o array estar rotacionado para a direita.
+int rotated_search(vector<int> &v){
     int n = (int)v.size();
     int low = 0;
     int high = n - 1;
     while(low <= high){
-        if(v[low] < v[high]) return low;
+        if(v[low] < v[high]) return low; //isso quer dizer que o array ja ta ordenado. Ou seja, basta eu retornar o indice "low"
         int mid = low + (high - low)/2;
+        int prev = (mid + n - 1) % n; 
         int next = (mid + 1) % n;
-        int prev = (mid + n - 1) % n;
-        if(v[prev] > v[mid] && v[mid] < v[next]) return mid;
-        else if(v[low] <= v[mid]){
-            low = mid + 1;
-        }else if(v[mid] <= v[high]){
-            high = mid - 1;
-        }
+        if(v[mid] < v[prev] && v[mid] < v[next]) return mid;//nesse caso, achei o pivot (que eh quem eu to procurando). retorno o indice dele.
+        if(v[low] < v[mid]) low = mid + 1; //corto o espaco de busca
+        if(v[mid] < v[high]) high = mid - 1; //corto o espaco de busca
     }
-    return -1; //case of invalid input vector
+    return -1; //recebi um input invalido.
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    vector<int> v = {2,3,4,5,7};
-    
-    int num_rotations = find_rotation(v);
-    cout << num_rotations << "\n";
-    
+    vector<int> v = {1,23,34,55,67,74}; 
+    int rot = rotated_search(v);
+    if(rot == -1){
+        cout << "The array provided in the input is invalid." << "\n";
+    }else{
+        cout << "The array got rotated: " << rot << " times." <<"\n";
+    }
     return 0;
 }
