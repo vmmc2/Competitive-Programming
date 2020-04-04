f315d71c18edd1783e7252dbd383b1ee4824af74
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-04 15:07:54 -0700

    Update How Many Times Is A Sorted Array Rotated?.cpp

--- Cracking The Code/Chapter 10 (Sorting and Searching)/How Many Times Is A Sorted Array Rotated?.cpp
+++ Cracking The Code/Chapter 10 (Sorting and Searching)/How Many Times Is A Sorted Array Rotated?.cpp
@@ -1,37 +1,39 @@
 #include <bits/stdc++.h>
-#include <sstream>
-#include <cmath>
 
 using namespace std;
 
-
-//O algoritmo abaixo so funciona no caso de o array estar rotacionado para a direita.
-int rotated_search(vector<int> &v){
+int rotated_array(vector<int> &v){ //tem que achar o index onde temos o ponto de vale ("valley point").
     int n = (int)v.size();
     int low = 0;
     int high = n - 1;
     while(low <= high){
-        if(v[low] < v[high]) return low; //isso quer dizer que o array ja ta ordenado. Ou seja, basta eu retornar o indice "low"
+        if(v[low] <= v[high]) return low;
         int mid = low + (high - low)/2;
-        int prev = (mid + n - 1) % n; 
+        int prev = (mid + n - 1) % n;
         int next = (mid + 1) % n;
-        if(v[mid] < v[prev] && v[mid] < v[next]) return mid;//nesse caso, achei o pivot (que eh quem eu to procurando). retorno o indice dele.
-        if(v[low] < v[mid]) low = mid + 1; //corto o espaco de busca
-        if(v[mid] < v[high]) high = mid - 1; //corto o espaco de busca
+        //checagem para o ponto de vale.
+        if(v[prev] >= v[mid] && v[mid] <= v[next]) return mid;
+        if(v[low] <= v[mid]){
+            low = mid + 1;
+        }
+        if(v[mid] <= v[high]){
+            high = mid - 1;
+        }
     }
-    return -1; //recebi um input invalido.
+    return -1;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    vector<int> v = {1,23,34,55,67,74}; 
-    int rot = rotated_search(v);
-    if(rot == -1){
-        cout << "The array provided in the input is invalid." << "\n";
+    vector<int> v = {11,12,13,15,17,20,1};
+    int how_many = rotated_array(v);
+    if(how_many == -1){
+        cout << "invalid array provided in the input." << "\n";
     }else{
-        cout << "The array got rotated: " << rot << " times." <<"\n";
+        cout << "the array was rotated: " << how_many << " times." << "\n";
     }
+    
     return 0;
 }
