1170b091d06a4a8d730827ac0f696df2800eb594
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-09 16:24:18 -0700

    Update Rotate Image.cpp

--- LeetCode/Top Interview Questions/Easy Collection/Array/Rotate Image.cpp
+++ LeetCode/Top Interview Questions/Easy Collection/Array/Rotate Image.cpp
@@ -16,3 +16,26 @@ public:
     
     }
 };
+
+/*************************************************************************************/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        if((int)matrix.size() == 0) return;
+        if((int)matrix[0].size() == 0) return;
+        int n = (int)matrix.size();
+        for(int layer = 0; layer < n/2; layer++){
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int i = first; i < last; i++){
+                int offset = i - first;
+                int temp = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = temp;
+            }
+        }
+        return;
+    }
+};
