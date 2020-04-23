dcd970c1b218feeeb4efb48af684dd6da59a7004
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-04-22 20:22:16 -0700

    Update mock5.cpp

--- Mock/mock5.cpp
+++ Mock/mock5.cpp
@@ -1,3 +1,88 @@
+// classic problem
+input = "cafeteria", "cafe"
+  			"cafe"   answer = 0
+  
+// your problem
+input = "maquina" "quma"
+        "quma" answer = 0
+
+"maqu" is an anagram of "quma", so answer = 0
+
+
+  
+mbquina
+
+m -> 1
+b -> 1
+
+
+quma
+size = 4
+q -> 1
+u -> 1
+m -> 1
+a -> 1
+b -> 0
+  
+  
+bool check_hashtable(unordered_map<char,int> &hashtable, unordered_map<char,int> &hs){
+  for (auto [key, value] : hs) {
+    //
+  }
+  
+  for(auto it = hashtable.begin(); it != hashtable.end(); it++){
+			if(hs.find(it->first) == hs.end()){
+        return false;
+      }else{
+        if(it->second != hs[it->first]) return false;
+      }
+  }
+  return true;
+}
+
+O(52) + (s.size() - anam.size())*O(52) = O(1)  
+  
+O((s.size() - anam.size()) * (anam.size()) = O(N * M) ~ O(N²)
+  
+int check_anagram(string s, string anam){
+  //treating the corner cases
+  if(anam == "" && s == "") return 0;
+  
+  int n = (int)anam.size();
+  unordered_map<char,int> hashtable;
+  for(int i = 0; i < n; i++){
+    hashtable[anam[i]]++;
+  }  
+  int tam = (int)s.size();
+  for(int cur = 0; cur <= tam - n ; cur++){
+    unordered_map<char,int> hs;
+    int c1 = cur;
+    int s1 = 1;
+    int flag = 1; // assuming that i can found an anagram starting at cur
+    while(s1 <= n){
+      hs[s[c1]]++; //building our hashtable
+      if(hashtable[s[c1]] == 0  || hs[s[c1]] > hashtable[s[c1]]){
+        flag = 0;
+        break;
+      }
+      s1++;
+      c1++;
+    }
+    if(flag == 0){
+      continue;
+    }else{
+      if(check_hashtable(hashtable, hs)){
+				return cur;
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+
+===============================================================================================
 input = [1, 5, 10, 9, 6, 21], 4
 
 maxi = 21
