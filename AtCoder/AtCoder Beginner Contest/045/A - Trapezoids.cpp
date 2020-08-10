08e60d10961faa65414f05b1ac5948500c32aab1
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-08-10 13:12:34 -0700

    Create A - Trapezoids.cpp

---
+++ AtCoder/AtCoder Beginner Contest/045/A - Trapezoids.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int a, b, h;
+    cin >> a;
+    cin >> b;
+    cin >> h;
+    
+    int answer = ((a+b)*h)/2;
+    cout << answer << endl;
+    
+    
+    return 0;
+}
