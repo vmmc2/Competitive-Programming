1a5351a2363e003d8a3f1c8b4d624aca0d2482d3
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-10-20 09:11:51 -0300

    Create A - AtCoder *** Contest.cpp

---
+++ AtCoder/AtCoder Beginner Contest/048/A - AtCoder *** Contest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    string a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    cout << a[0] << b[0] << c[0] << endl;
+    
+    return 0;
+}
