1734402c452598c3dc6fe9f4a0fb81af07fc5caa
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-11-01 00:38:46 -0300

    Update D - Connectivity.cpp

--- AtCoder/AtCoder Beginner Contest/049/D - Connectivity.cpp
+++ AtCoder/AtCoder Beginner Contest/049/D - Connectivity.cpp
@@ -3,20 +3,50 @@
 using namespace std;
 
 typedef long long ll;
+typedef unsigned long long ull;
 
-void dfs(int source, unordered_map<int,int> &visited, vector<vector<int>> &adjlist){
-    if(visited[source] == 1){
-        return;
-    }
-    visited[source] = 1;
-    for(int i = 0; i < (int)adjlist[source].size(); i++){
-        int x = adjlist[source][i];
-        if(visited[x] == 0){
-            dfs(x, visited, adjlist);
+class dsu{
+    private:
+        vector<int> root;
+        vector<int> size;
+    public:
+        dsu(int n){
+            size.assign(n, 1);
+            root.assign(n, 0);
+            for(int i = 0; i < n; i++){
+                root[i] = i;
+            }
         }
-    }
-    return;
-}
+        int findRoot(int i){
+            while(root[i] != i){
+                root[i] = root[root[i]];
+                i = root[i];
+            }
+            return root[i];
+        }
+        bool find(int a, int b){
+            int rootA = findRoot(a);
+            int rootB = findRoot(b);
+            if(rootA == rootB) return true;
+            else return false;
+        }
+        void unite(int a, int b){
+            int rootA = findRoot(a);
+            int rootB = findRoot(b);
+            if(size[rootA] <= size[rootB]){
+                root[rootA] = root[rootB];
+                size[rootB] += size[rootA];
+            }else{
+                root[rootB] = root[rootA];
+                size[rootA] += size[rootB];
+            }
+            return;
+        }
+        int subsetSize(int i){
+            int rootI = findRoot(i);
+            return size[rootI];
+        }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -25,49 +55,26 @@ int main(){
     int n, k, l;
     cin >> n >> k >> l;
     
-    vector<vector<int>> adjlistroad(n + 1);
-    vector<vector<int>> adjlistrail(n + 1);
-    unordered_map<int,int> visitedroad;
-    unordered_map<int,int> visitedrail;
-    vector<int> answer(n + 1, 0);
+    dsu uf1(n + 1);
+    dsu uf2(n + 1);
+    map<pair<int,int>,int> dicio;
     
     int from, to;
-    for(int i = 0; i < k; i++){
+    for(int i = 1; i <= k; i++){
         cin >> from >> to;
-        adjlistroad[from].push_back(to);
-        adjlistroad[to].push_back(from);
+        uf1.unite(from, to);
     }
-    for(int i = 0; i < l; i++){
+    for(int i = 1; i <= l; i++){
         cin >> from >> to;
-        adjlistrail[from].push_back(to);
-        adjlistrail[to].push_back(from);
+        uf2.unite(from, to);
     }
     
     for(int i = 1; i <= n; i++){
-        visitedroad.clear();
-        visitedrail.clear();
-        dfs(i, visitedroad, adjlistroad);
-        dfs(i, visitedrail, adjlistrail);
-        /*cout << "Partindo da cidade " << i << ": " << endl;
-        for(int k = 1; k <= n; k++){
-            cout << visitedroad[k] << " ";
-        }
-        cout << endl;
-        for(int k = 1; k <= n; k++){
-            cout << visitedrail[k] << " ";
-        }*/
-        /*cout << endl;
-        cout << endl;*/
-        for(unordered_map<int,int>::iterator it = visitedroad.begin(); it != visitedroad.end(); it++){
-            if(visitedrail[it->first] == 1){
-                answer[i]++;
-            }
-        }
-        
+        dicio[{uf1.findRoot(i), uf2.findRoot(i)}]++;
     }
-    
     for(int i = 1; i <= n; i++){
-        cout << answer[i] << " ";
+        if(i > 1) cout << " ";
+        cout << dicio[{uf1.findRoot(i), uf2.findRoot(i)}];
     }
     
     
