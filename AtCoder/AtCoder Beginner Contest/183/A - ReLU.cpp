1b26324e0d5c088402a9f403218f4a844a1cc3fe
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-12-09 22:38:06 -0300

    Create A - ReLU.cpp

---
+++ AtCoder/AtCoder Beginner Contest/183/A - ReLU.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int a;
+  cin >> a;
+  
+  if(a >= 0){
+  	cout << a << endl;
+  }else{
+  	cout << "0" << endl;
+  }
+  
+  return 0;
+}
