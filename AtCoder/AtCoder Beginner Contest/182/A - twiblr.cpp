a5b4423bfefe96af97bc63bd9660b8aff6ff534d
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-12-10 16:10:49 -0300

    Create A - twiblr.cpp

---
+++ AtCoder/AtCoder Beginner Contest/182/A - twiblr.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int a, b;
+  cin >> a >> b;
+  
+  cout << (2*a + 100) - b << endl;
+  return 0;
+}
