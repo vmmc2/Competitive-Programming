f14f65e70866ce29bbbecc90952ce30b0877fc8b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-12-16 16:22:41 -0300

    Create A - box.cpp

---
+++ AtCoder/AtCoder Beginner Contest/180/A - box.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  int n, a, b;
+  cin >> n >> a >> b;
+  cout << n - a + b << endl;
+  
+  return 0;
+}
