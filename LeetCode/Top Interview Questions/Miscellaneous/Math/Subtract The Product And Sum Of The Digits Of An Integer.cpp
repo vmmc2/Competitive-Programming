df0b8e5eb624656e5f194d7c27f946fd9e6b3fd1
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-12-26 16:49:05 -0300

    Create Subtract The Product And Sum Of The Digits Of An Integer.cpp

---
+++ LeetCode/Top Interview Questions/Miscellaneous/Math/Subtract The Product And Sum Of The Digits Of An Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int answer, a = 1, b = 0;
+        while(n != 0){
+            int digit = n % 10;
+            a = a * digit;
+            b = b + digit;
+            n = n/10;
+        }
+        answer = a - b;
+        return answer;
+    }
+};
