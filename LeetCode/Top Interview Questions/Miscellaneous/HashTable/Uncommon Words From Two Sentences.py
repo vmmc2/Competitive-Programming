fffe46d1c4a4597698d65139e145d835b7a4a738
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-12-26 13:13:59 -0300

    Create Uncommon Words From Two Sentences.py

---
+++ LeetCode/Top Interview Questions/Miscellaneous/HashTable/Uncommon Words From Two Sentences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uncommonFromSentences(self, A: str, B: str) -> List[str]:
+        l1 = A.split(" ")
+        l2 = B.split(" ")
+        dA = Counter(l1)
+        dB = Counter(l2)
+        answer = []
+        
+        for k,v in dA.items():
+            if v == 1 and k not in dB:
+                answer.append(k)
+        for k,v in dB.items():
+            if v == 1 and k not in dA:
+                answer.append(k)
+        
+        return answer
+        
+        
