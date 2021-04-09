63f8ddbfe9f9923bc56fec31e6ef2db2de393615
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-04-09 11:32:43 -0300

    Update dfs_find_diameter_of_tree.cpp

--- The Notebook/Graphs/Depth First Search/dfs_find_diameter_of_tree.cpp
+++ The Notebook/Graphs/Depth First Search/dfs_find_diameter_of_tree.cpp
@@ -24,7 +24,14 @@ mais longe dessa segunda origem e sua respectiva distancia eh o diametro da arvo
 using namespace std;
 
 void dfs(int source, vector<vector<int>> &adjlist, vector<bool> &visited, vector<int> &dist){
-    
+    visited[source] = true;
+    for(int i = 0; i < (int)adjlist[source].size(); i++){
+        int u = adjlist[source][i];
+        if(visited[u] == false){
+            dist[u] = dist[source] + 1;
+            dfs(u, adjlist, visited, dist);
+        }
+    }
     return;
 }
 
@@ -32,7 +39,37 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    
+    int a, b;
+    int vertex, edges;
+    cin >> vertex >> edges;
+
+    vector<vector<int>> adjlist(vertex, vector<int>());
+    vector<bool> visited(vertex, false);
+    vector<int> dist(vertex, 0);
+
+    for(int x = 0; x < edges; x++){
+        cin >> a >> b;
+        adjlist[a].push_back(b);
+        adjlist[b].push_back(a);
+    }
+    int firstRoot = 0;
+    int secondRoot, maxDist = -1;
+    dfs(firstRoot, adjlist, visited, dist);
+    for(int i = 0; i < vertex; i++){
+        if(dist[i] > maxDist){
+            maxDist = dist[i];
+            secondRoot = i;
+        }
+    }
+    visited.assign(vertex, false);
+    dist.assign(vertex, 0);
+    dfs(secondRoot, adjlist, visited, dist);
+    int diameter = -1;
+    for(int i = 0; i < vertex; i++){
+        diameter = max(diameter, dist[i]);
+    }
+
+    cout << "The diameter of the tree is: " << diameter << endl;
 
     return 0;
 }
