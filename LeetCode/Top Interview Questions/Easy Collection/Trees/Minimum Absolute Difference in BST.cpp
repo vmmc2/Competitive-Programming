8efea19b9fecfdd5a1d6680f759eb811c4dd153e
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2021-07-14 22:44:22 -0300

    Create Minimum Absolute Difference in BST.cpp

---
+++ LeetCode/Top Interview Questions/Easy Collection/Trees/Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorderTraversal(TreeNode* root, vector<int> &values){
+        if(root == NULL) return;
+        
+        inorderTraversal(root->left, values);
+        values.push_back(root->val);
+        inorderTraversal(root->right, values);
+        
+        return;
+    }
+    int getMinimumDifference(TreeNode* root) {
+        //ideia: fazer traversia inorder guardando num vector e depois percorre o vector para achar a menor diferenca
+        vector<int> values;
+        inorderTraversal(root, values);
+        
+        int answer = values.back() - values[0];
+        for(int i = 1; i < (int)values.size(); i++){
+            answer = min(answer, values[i] - values[i - 1]);
+        }
+        return answer;
+    }
+};
