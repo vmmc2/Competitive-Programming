8199725beb34224abda2e7a5f0b251c2a9d901a1
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2022-01-26 22:37:44 -0300

    Update and rename 208A - (not done) Dubstep.cpp to 208A - Dubstep.cpp

--- Codeforces/Ladders/Ladder 1600-1699/208A - Dubstep.cpp
+++ Codeforces/Ladders/Ladder 1600-1699/208A - Dubstep.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 #include <sstream>
-
+ 
 using namespace std;
-
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
+ 
     int curr = 0, len;
     string s;
     vector<string> v;
-    string c = "";
-
+    string c = "", answer = "";
+ 
     cin >> s;
-
+ 
     len = (int)s.size();
-
+ 
     while(curr < len){
         if(curr <= len - 3){
             if(s[curr] == 'W' && s[curr + 1] == 'U' && s[curr + 2] == 'B'){
-                v.push_back(c);
+                if(c != "") v.push_back(c);
                 c = "";
                 curr += 3;
             }else{
@@ -30,19 +30,18 @@ int main(){
             c += s[curr];
             curr += 1;
         }
-
     }
-    c = "";
+    if(c != "") v.push_back(c);
+ 
     for(int i = 0; i < (int)v.size(); i++){
         if(i == (int)v.size() - 1){
-            c += v[i];
+            answer += v[i];
         }else{
-            c += v[i] + " ";
+            answer += v[i] + " ";
         }
-        cout << v[i] << endl;
     }
-    cout << endl;
-    cout << c << endl;
-
+ 
+    cout << answer << endl;
+ 
     return 0;
 }
