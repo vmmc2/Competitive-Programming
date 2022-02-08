fd193b9e01178dafe83cebced5bc8a0fd56166ce
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2022-02-07 23:36:45 -0300

    Update and rename (not done) - Palindrome Transformation.cpp to 486C - Palindrome Transformation.cpp

--- Codeforces/Ladders/Ladder 1600-1699/486C - Palindrome Transformation.cpp
+++ Codeforces/Ladders/Ladder 1600-1699/486C - Palindrome Transformation.cpp
@@ -73,24 +73,65 @@ int main(){
         }
     }else{
         middle = n/2;
+        //cout << middle << " " << s[middle] << endl;
+        int leftmostL = 0, rightmostR = n - 1;
+        int rightmostL = middle - 1, leftmostR = middle + 1; 
+        while(s[leftmostL] == s[rightmostR] && leftmostL <= middle - 1 && middle + 1 <= rightmostR){
+            leftmostL++;
+            rightmostR--;
+        }
+        while(s[rightmostL] == s[leftmostR] && rightmostL >= 0 && leftmostR <= n - 1){
+            rightmostL--;
+            leftmostR++;
+        }
+        if(leftmostL == rightmostR || (rightmostL < 0 && leftmostR > n - 1)){
+            cout << 0 << endl;
+            return 0;
+        }
+        //cout << rightmostL << " " << leftmostR << endl;
         
         if(initPos < middle){
-            answer += min(abs(0 - initPos)*2 + abs(middle - 1 - initPos), abs(0 - initPos) + abs(middle - 1 - initPos)*2);
-            for(int i = 0; i <= middle - 1; i++){
+            int begin, end;
+            if(initPos > rightmostL){
+                answer += abs(leftmostL - rightmostL) + abs(initPos - rightmostL);
+            }else if(initPos >= leftmostL && initPos <= rightmostL){
+                answer += min(abs(leftmostL - initPos)*2 + abs(rightmostL - initPos), abs(leftmostL - initPos) + abs(rightmostL - initPos)*2);
+            }else if(initPos < leftmostL){
+                answer += abs(leftmostL - rightmostL) + abs(initPos - leftmostL);
+            }
+            begin = leftmostL, end = rightmostL;
+            for(int i = begin; i <= end; i++){
                 char minLetter = (char)min((int)s[i], (int)s[n - 1 - i]);
                 char maxLetter = (char)max((int)s[i], (int)s[n - 1 - i]);
                 answer += min((int)maxLetter - (int)minLetter, ((int)minLetter - (int)'a') + ((int)'z' - (int)maxLetter) + 1);
             }
         }else if(initPos > middle){
-            answer += min(abs(middle + 1 - initPos)*2 + abs(n - 1 - initPos), abs(middle + 1 - initPos) + abs(n - 1 - initPos)*2);
-            for(int i = middle + 1, j = 0; i <= n - 1; i++, j--){
-                char minLetter = (char)min((int)s[i], (int)s[middle - 1 - j]);
-                char maxLetter = (char)max((int)s[i], (int)s[middle - 1 - j]);
+            int begin, end;
+            //cout << "entrei" << endl;
+            if(initPos > rightmostR){
+                answer += abs(leftmostR - rightmostR) + abs(initPos - rightmostR);
+                //cout << "entrei2" << endl;
+            }else if(initPos >= leftmostR && initPos <= rightmostR){
+                //cout << "entrei3" << endl;
+                answer += min(abs(leftmostR - initPos)*2 + abs(rightmostR - initPos), abs(leftmostR - initPos) + abs(rightmostR - initPos)*2);
+            }else if(initPos < leftmostR){
+                answer += abs(leftmostR - rightmostR) + abs(initPos - leftmostR);
+                //cout << "entrei4" << endl;
+            }
+            //cout << "answer1: " << answer << endl;
+            begin = leftmostR, end = rightmostR;
+            //cout << "begin: " << begin << " --- end: " << end << endl;
+            for(int i = begin, j = middle - abs(leftmostR - middle); i <= end; i++,  j--){
+                char minLetter = (char)min((int)s[i], (int)s[j]);
+                char maxLetter = (char)max((int)s[i], (int)s[j]);
                 answer += min((int)maxLetter - (int)minLetter, ((int)minLetter - (int)'a') + ((int)'z' - (int)maxLetter) + 1);
+                //cout << "answer2: " << answer << endl;
             }
         }else{
-            answer += middle;
-            for(int i = 0; i <= middle - 1; i++){
+            int begin, end;
+            answer += abs(middle - rightmostL) + abs(leftmostL - rightmostL);
+            begin = leftmostL, end = rightmostL;
+            for(int i = begin; i <= end; i++){
                 char minLetter = (char)min((int)s[i], (int)s[n - 1 - i]);
                 char maxLetter = (char)max((int)s[i], (int)s[n - 1 - i]);
                 answer += min((int)maxLetter - (int)minLetter, ((int)minLetter - (int)'a') + ((int)'z' - (int)maxLetter) + 1);
